4e7698d0c4144e5f9405b1e979a2f843617f30db
Gregory Petukhov <lorien@lorien.name>
2017-01-12 00:29:04 +0700

    Disable lxml dependency for windows platform

--- setup.py
+++ setup.py
@@ -10,7 +10,12 @@ setup(
     description = 'Set of tools for web scraping projects',
     author = 'Gregory Petukhov',
     author_email = 'lorien@lorien.name',
-    install_requires = ['pytils', 'six', 'lxml', 'user_agent'],
+    install_requires = [
+        'pytils',
+        'six',
+        'lxml;platform_system!="Windows"',
+        'user_agent',
+    ],
     packages = find_packages(exclude=['test']),
     license = "MIT",
     classifiers = (
